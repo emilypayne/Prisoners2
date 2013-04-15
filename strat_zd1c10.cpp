4b2680fcc11f28fb8489200c05103e84035ad5c1
Emily Payne <emily.s.payne@gmail.com>
2013-04-15 12:30:15 +0100

    Re adding files

---
+++ strat_zd1c10.cpp
@@ -0,0 +1,42 @@
+#include "strat_zd1c10.h"
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+strat_zd1c10::strat_zd1c10()
+{
+    //ctor
+}
+
+bool::strat_zd1c10::play(bool other_p, bool p, int round)
+{
+    if(round > 0)
+    {
+        if ((other_p == 1) && (p == 1))
+        {
+            return true;
+        }
+        else if((other_p == 1) && (p == 0))
+        {
+            int x = rand() % 100;
+            if(x <= 90)
+            {
+                return true;
+            }
+            else return false;
+        }
+        else if((other_p == 0) && (p == 1))
+        {
+            int x = rand() % 100;
+            if(x <= 40)
+            {
+                return true;
+            }
+            else return false;
+        }
+        else if((other_p == 0) && (p == 0))
+        {
+            return false;
+        }
+    }
+    else return true;
+}
